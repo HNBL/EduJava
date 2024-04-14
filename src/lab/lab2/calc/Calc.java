e1e809871d15605cf52f7f83ab40b0cf332f4b33
HNBL <DerKudinov@gmail.com>
2024-04-14 18:06:25 +0300

    update main lab1 -lab6

--- src/lab/lab2/calc/Calc.java
+++ src/lab/lab2/calc/Calc.java
@@ -1,4 +1,60 @@
 package lab.lab2.calc;
 
-public class Calc {
+class Calculate {
+
+    public static void main (String[] args){
+       System.out.println(Sum(956,6));
+        System.out.println(Minus(9.6,6));
+        System.out.println(Del(9.6,6));
+        System.out.println(Multi(9.6,665456655));
+    }
+
+
+    static int Sum(int a, int b) {
+      return a+b;
+    }
+    static double Sum(double a, double b) {
+        return a+b;
+    }
+    static long Sum(long a, long b) {
+        return  a+b;
+    }
+
+
+
+    static int Minus(int a, int b) {
+        return a-b;
+    }
+    static double Minus(double a, double b) {
+        return a-b;
+    }
+    static long Minus(long a, long b) {
+        return a-b;
+    }
+
+
+
+    static int Del(int a, int b) {
+        return  a / b;
+    }
+    static double Del(double a, double b) {
+        return  a / b;
+    }
+    static long Del(long a, long b) {
+        return a / b;
+    }
+
+
+    static int Multi(int a, int b)
+    {
+        return a*b;
+    }
+    static double Multi(double a, double b)
+    {
+        return a*b;
+    }
+    static long Multi(long a, long b)
+    {
+        return a*b;
+    }
 }
---
+++ src/lab/lab2/essence/Comp.java
@@ -0,0 +1,63 @@
+package lab.lab2.essence;
+
+public class Comp {
+
+    public static void main(String[] args) {
+        Computer Egor = new Computer();
+        Egor.computerInfo();
+
+        Computer Ivan = new Computer("AFOX IG41-MA7", "AMD FX-4300 BOX","KFA2 GeForce 210",1000,4 );
+        Ivan.computerInfo();
+
+        Computer Max = new Computer("Intel Celeron G6900 OEM");
+        Max.computerInfo();
+
+
+
+
+    }
+    }
+
+     class  Computer{
+
+        String mb;
+        String CPU;
+        String GPU;// имя
+        int GB;
+        int RAM;// возраст
+
+
+        Computer(String a,String b,String c, int n,int v){
+            mb =a;
+            CPU =b;
+            GPU = c;
+            GB=n;
+            RAM=v;
+
+        }
+
+        Computer(String b ){
+
+            CPU =b;
+        }
+
+        Computer(){
+
+        }
+
+         void computerInfo(){
+             System.out.printf("Матпала: %s\n процесор: %s\n видеокарта %s\n объем памяти %d\n оперативная %d\n", mb, CPU,GPU,GB,RAM);
+         }
+
+
+    }
+
+
+    //Egor.mb = "ASUS TUF GAMING Z790";
+//        Egor.GPU ="rtx4090";
+//        Egor.CPU ="i9-12900k";
+//        Egor.GB = 2000;
+//        Egor.RAM = 32;
+//        Egor.computerInfo();
+
+
